e08585828bc75710ecbfa3dac22508dc31f165ff
Sandyla <sanystocler>
2025-08-18 14:10:29 -0300

    Tentando corrigindo nnovo criar

--- obj/Debug/net8.0/2.asp.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/2.asp.net.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2.asp.net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d98aee1535f2bdaceb34a7701c5d62c920b383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c626d705a2fed50edda6f308da9b7acac987960")]
 [assembly: System.Reflection.AssemblyProductAttribute("2.asp.net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2.asp.net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
